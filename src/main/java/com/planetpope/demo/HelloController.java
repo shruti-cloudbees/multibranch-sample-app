8f20eb2ee7a8f4f1437d8e616375d8150d9dc996
shruti-cloudbees <106113675+shruti-cloudbees@users.noreply.github.com>
2022-09-19 11:06:55 -0400

    Update HelloController.java

--- src/main/java/com/planetpope/demo/HelloController.java
+++ src/main/java/com/planetpope/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String sayHello(){
+    public String sayHello() {
         return "Hey";
     }
 
